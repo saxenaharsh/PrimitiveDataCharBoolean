85983ca6b63ac98b43deb2b3347a2fc52b05b939
saxenaharsh <harsh.saxena@gmail.com>
2019-06-21 21:05:49 +0530

    Add files via upload

---
+++ PrimitiveDataCharBoolean/src/com/HarshSaxena/Main.java
@@ -0,0 +1,16 @@
+package com.HarshSaxena;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        char myChar = '\u00A9';
+        System.out.println("Unicode char is " + myChar);
+
+        boolean isMale = true;
+        System.out.println("Boolean value is " + isMale);
+
+        
+        
+    }
+}
